16a9b217ed599a415ff567af59dc8d8a8428fbcd
Jérémie PIARD <jeremie.piard@yahoo.fr>
2021-08-01 03:21:42 +0200

    test debug fetch

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,20 @@ import Photographer from './photographer.js';
 
 let data = {};
 
-fetch("../data/FishEyeData.json")
+fetch("https://raw.githubusercontent.com/jeireme/JeremiePiard_6_27072021/master/data/FishEyeData.json")
     .then(response => response.json())
     .then(json => {
         displayPhotographers(json.photographers);
-        displayMedias(json.media);
+        //displayMedias(json.media);
     });
 
 function displayPhotographers(photographers) {
     for (let i in photographers) {
-        console.log(i);
         let photographer = new Photographer(photographers[i], i);
         photographer.display();
     }
 }
 
-function displayMedias(medias) {
-    console.log("Liste des médias : ", medias);
-}
+// function displayMedias(medias) {
+//     console.log("Liste des médias : ", medias);
+// }
